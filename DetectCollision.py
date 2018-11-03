316422c1fce3e6e0319609adf899861ebf8846b1
rbhog <rbhogavilli@gmail.com>
2018-11-02 20:22:14 -0400

    Lighten image for edge detection

--- DetectCollision.py
+++ DetectCollision.py
@@ -1,8 +1,14 @@
 import cv2
 import numpy as np
+import Image
 from matplotlib import pyplot as plt
 
-img = cv2.imread('diff1.png',0)
+#lightens the image for better edges
+original = Image.open("diff1.png")
+lightened = original.point(lambda p: p * 0.9)
+im2.save("lightened_difference.png")
+
+img = cv2.imread('lightened_difference.png',0)
 edges = cv2.Canny(img,100,200)
 
 plt.subplot(121),plt.imshow(img,cmap = 'white')
